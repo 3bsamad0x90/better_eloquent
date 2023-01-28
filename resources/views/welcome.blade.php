e04ec6ca45aeedf174e191c78f5b44c3fb5939fc
3bsamad0x90 <mohamedabdelsamad122@gmail.com>
2023-01-28 22:26:49 +0200

    inital commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('title')
+    Better Eloquent performance
+@endsection
+@section('content')
+    <h1 class="text-center">Better Eloquent performance</h1>
+<table class="table">
+    <thead class="thead-dark text-center">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Title</th>
+        <th scope="col">Auther</th>
+        <th scope="col">Email</th>
+      </tr>
+    </thead>
+    <tbody class="text-center">
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+    </tbody>
+  </table>
+@endsection
